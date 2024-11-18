98af8f16091f582c2e91a20cd358812b51d041a6
ahuytoichoi <huy54212410028@bachkhoasaigon.edu.vn>
2024-11-18 14:13:48 +0700

    Add project files.

---
+++ bai15_array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{ // create an array of strings
+	string cars[5] = {"volvo","bmw","ford","mazda","tesla"};
+	// loop through strings
+	for (int i = 0; i < 5; i++) {
+		cout << cars[i] << "\n";
+	}
+	return 0;
+	{
+
+	}
+}
